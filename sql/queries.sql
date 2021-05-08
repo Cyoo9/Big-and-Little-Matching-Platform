3cfb20fe092b63511d6e828f39693742a93e2138
Caleb Yoo <calebyoo33@gmail.com>
2021-05-07 20:00:24 -0700

    google auth fix

--- sql/queries.sql
+++ sql/queries.sql
@@ -7,8 +7,11 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users
 (
 	id SERIAL,
-	name VARCHAR(255) NOT NULL,
-	biglittle VARCHAR(255) NOT NULL,
+	name VARCHAR(255),
+	biglittle VARCHAR(255),
+	hobbylist VARCHAR(255),
+	yr VARCHAR(255),
+	major VARCHAR(255), 
 	email VARCHAR(255) NOT NULL,
     password VARCHAR(255),
     seeking VARCHAR(255),

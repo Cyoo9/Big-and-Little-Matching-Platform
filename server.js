53b0976ca5fc75d69c192118ec3e0f5de6a17b07
Jeff <nguyen.jeffreyson@gmail.com>
2021-05-23 12:49:53 -0700

    -Modfied Like to AJAX

--- server.js
+++ server.js
@@ -43,23 +43,22 @@ app.get('/home', function (req, res) {
     res.render('index');
 });
 
-app.post('/captcha', function(req, res) {
-  if(req.body === undefined || req.body === '' || req.body === null)
-  {
-    return res.json({"responseError" : "captcha error"});
-  }
-  var secretKey = "6LdGcdsaAAAAAP6CFbVhB5E92nyuNmlDTvz049L8";
- 
-  const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.socket.remoteAddress;  
-  request(verificationURL,function(error,response,body) {
-    body = JSON.parse(body);
-    //console.log(body);
-    if(body.success) {
-        res.redirect('/home');
-    } else {
-      return res.json({"responseError" : "Failed captcha verification"});
+app.post('/captcha', function (req, res) {
+    if (req.body === undefined || req.body === '' || req.body === null) {
+        return res.json({ "responseError": "captcha error" });
     }
-  });
+    var secretKey = "6LdGcdsaAAAAAP6CFbVhB5E92nyuNmlDTvz049L8";
+
+    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.socket.remoteAddress;
+    request(verificationURL, function (error, response, body) {
+        body = JSON.parse(body);
+        //console.log(body);
+        if (body.success) {
+            res.redirect('/home');
+        } else {
+            return res.json({ "responseError": "Failed captcha verification" });
+        }
+    });
 });
 
 app.get('/users/login', checkAuthenticated, (req, res) => {
@@ -73,40 +72,42 @@ app.get('/users/register', checkAuthenticated, (req, res) => {
 app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
     User = req.user;
     res.render('dashboard', { user: req.user.name });
-    username = req.user.name; 
+    username = req.user.name;
     major = req.user.major;
     year = req.user.yr;
 });
 
 app.get('/users/profile', checkNotAuthenticated, (req, res) => {
 
-    if(googleUser == true) {
+    if (googleUser == true) {
         pool.query(
             `SELECT * FROM users 
             WHERE email = $1`, [userProfile.emails[0].value], function (err, results) {
             if (err) {
                 throw err;
             }
-            res.render('profile', { name: results.rows[0].name, 
+            res.render('profile', {
+                name: results.rows[0].name,
                 biglittle: results.rows[0].biglittle,
                 hobbylist: results.rows[0].hobbylist,
                 yr: results.rows[0].yr,
                 major: results.rows[0].major,
                 email: results.rows[0].email,
                 numLikes: results.rows[0].numlikes
-              });
+            });
         }
         );
     }
     else {
-        res.render('profile', { name: req.user.name, 
+        res.render('profile', {
+            name: req.user.name,
             biglittle: req.user.biglittle,
             hobbylist: req.user.hobbylist,
             yr: req.user.yr,
             major: req.user.major,
             email: req.user.email,
             numLikes: req.user.numlikes
-          });
+        });
     }
 
 });
@@ -121,20 +122,20 @@ app.get('/users/logout', (req, res) => {
 app.post('/users/profile/changeinfo/', checkNotAuthenticated, async (req, res) => {
     let name, hashedPassword, biglittle, hobbies, year, major, email;
 
-    if(req.body.name != "") name = req.body.name;
+    if (req.body.name != "") name = req.body.name;
     else name = req.user.name;
-    if(req.body.biglittle != "")  biglittle = req.body.biglittle; 
-    else biglittle = req.user.biglittle 
-    if(req.body.hobbies != "") hobbies = req.body.hobbies; 
+    if (req.body.biglittle != "") biglittle = req.body.biglittle;
+    else biglittle = req.user.biglittle
+    if (req.body.hobbies != "") hobbies = req.body.hobbies;
     else hobbies = req.user.hobbies;
-    if(req.body.year != "") year = req.body.year; 
+    if (req.body.year != "") year = req.body.year;
     else year = req.user.year;
-    if(req.body.major != "") major = req.body.major;
+    if (req.body.major != "") major = req.body.major;
     else major = req.user.major;
-    if(req.body.email != "") email = req.body.email; 
+    if (req.body.email != "") email = req.body.email;
     else email = req.user.email;
-    if(req.body.password != "") hashedPassword = await bcrypt.hash(req.body.password, 10);
-    else hashedPassword = req.user.password; 
+    if (req.body.password != "") hashedPassword = await bcrypt.hash(req.body.password, 10);
+    else hashedPassword = req.user.password;
 
     /*let errors = [];
 
@@ -160,13 +161,13 @@ app.post('/users/profile/changeinfo/', checkNotAuthenticated, async (req, res) =
     )*/
 
     var tempEmail;
-    if(googleUser == true) {
+    if (googleUser == true) {
         tempEmail = userProfile.emails[0].value;
     }
-    if(googleUser == false) {
+    if (googleUser == false) {
         tempEmail = User.email;
     }
-    if(tempEmail != email) {
+    if (tempEmail != email) {
         pool.query(
             'UPDATE users SET external_id = 0 WHERE email = $1;', [tempEmail], (err, results) => {
                 if (err) {
@@ -175,7 +176,7 @@ app.post('/users/profile/changeinfo/', checkNotAuthenticated, async (req, res) =
             }
         );
     }
-        pool.query(
+    pool.query(
         `UPDATE users
          SET 
             name = $1, 
@@ -187,20 +188,20 @@ app.post('/users/profile/changeinfo/', checkNotAuthenticated, async (req, res) =
             password = $7
             WHERE email = $8;
             `, [name, biglittle, hobbies, year, major, email, hashedPassword, tempEmail], (err, results) => {
-                if (err) {
-                    throw err;
-                }
-                //console.log(results.rows);
-                req.flash('success_msg', "Profile change complete!");
-                if(googleUser == true) {
-                    userProfile.emails[0].value = email;
-                    res.redirect('/googleusers/dashboard');
-                }
-                else {
-                    res.redirect('/users/dashboard');
-                }
-            }
-        )
+        if (err) {
+            throw err;
+        }
+        //console.log(results.rows);
+        req.flash('success_msg', "Profile change complete!");
+        if (googleUser == true) {
+            userProfile.emails[0].value = email;
+            res.redirect('/googleusers/dashboard');
+        }
+        else {
+            res.redirect('/users/dashboard');
+        }
+    }
+    )
 });
 
 app.post('/users/register', async (req, res) => {
@@ -306,7 +307,7 @@ app.get('/googleusers/dashboard', (req, res) => {
             throw err;
         }
         console.log(results.rows);
-        res.render('dashboard', {user: results.rows[0].name});
+        res.render('dashboard', { user: results.rows[0].name });
     }
     );
 });
@@ -388,13 +389,13 @@ app.get('/auth/google/callback',
         // Successful authentication, redirect success.
         console.log("Successful Google Login");
         googleUser = true;
-        if(isNewUser == true) {
+        if (isNewUser == true) {
             res.redirect('/users/setpw');
         }
         else {
             res.redirect('/googleusers/dashboard');
         }
-});
+    });
 
 app.post('/users/setpassword', async (req, res) => {
     var name = userProfile.displayName;
@@ -441,8 +442,8 @@ app.post('/users/setpassword', async (req, res) => {
                         res.redirect('/googleusers/dashboard');
                     }
                 )
-            } 
             }
+        }
         );
     }
 });
@@ -454,13 +455,13 @@ app.post('/users/setpassword', async (req, res) => {
 //searches and filters users based on name/username
 
 
-app.get('/search',async function(req,res){  
+app.get('/search', async function (req, res) {
 
-    const {keyword} = req.query;
+    const { keyword } = req.query;
 
-    pool.query (
-        `SELECT name, biglittle, hobbylist, yr, major, email, numLikes FROM Users WHERE biglittle LIKE $1 or name = $2;`
-        ,['%'+keyword+'%', keyword], 
+    pool.query(
+        `SELECT name, biglittle, hobbylist, yr, major, email, numLikes FROM Users WHERE biglittle LIKE $1 or name LIKE $2;`
+        , ['%' + keyword + '%', '%' + keyword + '%'],
         (err, results) => {
             if (err) {
                 throw err;
@@ -475,11 +476,11 @@ app.get('/search',async function(req,res){
     )
 });
 
-app.get('/showuser', function(req, res) {
+app.get('/showuser', function (req, res) {
     console.log("Showing user info...");
 
-    pool.query (
-        'SELECT name, biglittle, hobbylist, yr, major, email, reputation, numLikes FROM USERS WHERE email = $1;', [req.query.prof], 
+    pool.query(
+        'SELECT name, biglittle, hobbylist, yr, major, email, reputation, numLikes FROM USERS WHERE email = $1;', [req.query.prof],
         (err, results) => {
             if (err) {
                 throw err;
@@ -495,34 +496,95 @@ app.get('/showuser', function(req, res) {
 });
 
 
-app.get('/showuser/like', function(req, res) {
+app.get('/showuser/like', function (req, res) {
 
     let reputation = "Unknown";
+    var numLikes;
 
-    let numLikes = parseInt(req.query.like);
-    numLikes++;
-
-    if(numLikes == 5) {
-        reputation = "Gaining Attraction"
-    } 
-    if(numLikes == 10) {
-        reputation = "Popular"
-    }
-    if(numLikes >= 15) {
-        reputation = "Very Popular"
-    }
-
-    pool.query (
-        `UPDATE users 
-        SET
-        numlikes = $1,
-        reputation = $2
-        WHERE email = $3;`, [numLikes, reputation, req.query.prof], 
-        (err) => {
-            if(err) {
+    pool.query(
+        'SELECT numlikes FROM USERS WHERE email = $1;', [req.query.email],
+        (err, results) => {
+            if (err) {
                 throw err;
             }
+            numLikes = results.rows[0].numlikes;
+            numLikes++;
+
+            if (numLikes == 5) {
+                reputation = "Gaining Attraction"
+            }
+            if (numLikes == 10) {
+                reputation = "Popular"
+            }
+            if (numLikes >= 15) {
+                reputation = "Very Popular"
+            }
+
+            pool.query(
+                `UPDATE users 
+                SET
+                numlikes = $1,
+                reputation = $2
+                WHERE email = $3;`, [numLikes, reputation, req.query.email],
+                (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                }
+            );
+            console.log("Liked User");
         }
-    );   
-    res.send("User Liked");
+    )
 });
+
+app.get('/showuser/unlike', function (req, res) {
+
+    let reputation = "Unknown";
+    var numLikes;
+
+    pool.query(
+        'SELECT numlikes FROM USERS WHERE email = $1;', [req.query.email],
+        (err, results) => {
+            if (err) {
+                throw err;
+            }
+            numLikes = results.rows[0].numlikes;
+            numLikes--;
+
+            if (numLikes == 5) {
+                reputation = "Gaining Attraction"
+            }
+            if (numLikes == 10) {
+                reputation = "Popular"
+            }
+            if (numLikes >= 15) {
+                reputation = "Very Popular"
+            }
+
+            pool.query(
+                `UPDATE users 
+                SET
+                numlikes = $1,
+                reputation = $2
+                WHERE email = $3;`, [numLikes, reputation, req.query.email],
+                (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                }
+            );
+            console.log("Unliked User");
+        }
+    )
+});
+/*End of search feature*/
+
+
+
+/*Start of comment feature*/
+
+
+
+/*End of comment feature*/
+
+
--- sql/queries.sql
+++ sql/queries.sql
@@ -1,9 +1,7 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS COMMENTS CASCADE;
 
 
-------------
----TABLES---
-------------
 CREATE TABLE users
 (
 	id SERIAL,
@@ -19,5 +17,49 @@ CREATE TABLE users
 	numlikes INTEGER,
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE like_users(
+	userid INTEGER,
+	user_liked_email VARCHAR(255),
+	FOREIGN KEY (userid) REFERENCES users(id)
+);
+
+CREATE TABLE matches (
+	userid INTEGER,
+	match_user_email VARCHAR(255),
+	match_user_name VARCHAR(255),
+	match_user_biglittle VARCHAR(255),
+	FOREIGN KEY (userid) REFERENCES users(id)
+)
+
+CREATE INDEX user_id_index ON users USING BTREE(id);
+CREATE INDEX user_name_index ON users USING BTREE(name);
+CREATE INDEX user_biglittle_index ON users USING BTREE(biglittle);
+CREATE INDEX user_hobbylist_index ON users USING BTREE(hobbylist);
+CREATE INDEX user_yr_index ON users USING BTREE(yr);
+CREATE INDEX user_major_index ON users USING BTREE(major);
+CREATE INDEX user_email_index ON users USING BTREE(email);
+CREATE INDEX user_password_index ON users USING BTREE(password);
+CREATE INDEX user_external_id_index ON users USING BTREE(external_id);
+CREATE INDEX user_reputation_index ON users USING BTREE(reputation);
+CREATE INDEX user_numlikes_index ON users USING BTREE(numlikes);
+
+CREATE INDEX like_users_userid_index ON like_users USING BTREE(userid);
+CREATE INDEX like_users_index ON like_users USING BTREE(user_liked_email);
+
+CREATE INDEX matches_userid_index ON matches USING BTREE(userid);
+CREATE INDEX match_user_email_index ON matches USING BTREE(match_user_email);
+CREATE INDEX match_user_name_index ON matches USING BTREE(match_user_name);
+CREATE INDEX match_user_biglittle_index ON matches USING BTREE(match_user_biglittle);
+
+
+
+
+
+
+
+
+
+
 
 
